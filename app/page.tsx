6427f5209656e28909a6172b5cca6c74d9d03acb
Resmini Loris <resminiloris@gmail.com>
2024-04-19 15:32:11 +0200

    added background and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
 
 export default function Home() {
   return (
-    <main>Home</main>
+    <main className="w-screen h-screen relative">
+      <div className="flex items-center w-full h-full bg-cover bg-center" style={{ backgroundImage: "url(/main-bg.jpg)" }}>
+        <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[800px]">
+          {/* slogan */}
+          <h1 className="text-[30px] text-white font-semibold">
+            {`Breathe life into your app, `}
+            <div className="text-[60px] text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-yellow-500">
+              one pixel at a time.
+            </div>
+          </h1>
+        </div>
+      </div>
+    </main>
   );
 }
